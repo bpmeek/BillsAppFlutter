e5a4f08799ce865c3936990a1bd787220d012eb4
dev <bpmeek.developer@gmail.com>
2020-04-15 16:36:36 -0500

    Removing Configs

--- lib/main_paid.dart
+++ lib/main_paid.dart
@@ -1,4 +1,4 @@
-
+import 'package:billsappflutter/resources/AppConfig.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -11,6 +11,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
